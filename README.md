c36e20967821ee3466784db5a277361c3e47928e
Wilver Vargas Anagua <kryshot05@gmail.com>
2022-10-14 17:41:08 -0400

    add validation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rust_learning
+<!-- https://dev.to/davidedelpapa/rocket-tutorial-04-data-persistency-and-rocket-with-mongodb-e5j -->
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate rocket;
+
 // ===========
+use chrono::Datelike;
 use rocket::request::{self, FromRequest, Outcome, Request};
 use rocket::{
     get,
@@ -10,14 +12,82 @@ use rocket_okapi::okapi::schemars;
 use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
 use rocket_okapi::{openapi, openapi_get_routes};
 use schemars::JsonSchema;
+// use validator::{Validate, ValidationError};
+use rocket_validation::{Validate, Validated};
 
-// ===========
-#[openapi(tag = "Users")]
-#[get("/getData")]
-fn index() -> &'static str {
-    "Hello, world!"
+// ======Root service=====
+#[derive(Serialize, Deserialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+struct ResponseI {
+    error: bool,
+    status: i32,
+    message: String,
+    response: Option<String>,
 }
 
+#[openapi(tag = "Servicio")]
+#[get("/")]
+fn index() -> Option<Json<ResponseI>> {
+    let year = chrono::Utc::now().year();
+
+    Some(Json(ResponseI {
+        error: false,
+        status: 200,
+        message: format!("Bienvenido al Servicio de Almacenamiento de Archivos, Basado en Principios REST, Copyright © Ministerio Público {year}"),
+        response: None
+    }))
+}
+
+// ========service for save files=============
+// #[derive(Serialize, Deserialize, JsonSchema)]
+#[derive(Debug, Deserialize, Serialize, Validate, JsonSchema)]
+#[serde(crate = "rocket::serde")]
+struct InputData {
+    #[validate(length(min = 1))]
+    appName: String,
+    nas: String,
+    path: String,
+    fileName: u16,
+    #[validate(range(min = 0, max = 10))]
+    age: u8,
+    base64: String,
+    withDateSubFolder: bool,
+}
+
+#[openapi(tag = "Archivo")]
+#[post("/write/base64", data = "<input>")]
+fn write_base64(input: Json<InputData>) -> Option<Json<ResponseI>> {
+    let year = chrono::Utc::now().year();
+
+    Some(Json(ResponseI {
+        error: false,
+        status: 200,
+        message: format!("Bienvenido al Servicio de Almacenamiento de Archivos, Basado en Principios REST, Copyright © Ministerio Público {year}"),
+        response: None
+    }))
+}
+
+// ========validation
+#[derive(Debug, Deserialize, Serialize, Validate)]
+///  Implements `Validate`
+#[serde(crate = "rocket::serde")]
+pub struct HelloData {
+    #[validate(length(min = 1))]
+    ///  Your validation annotation
+    name: String,
+    #[validate(range(min = 0, max = 100))]
+    ///  Your validation annotation
+    age: u8,
+}
+
+#[post("/hello", format = "application/json", data = "<data>")]
+fn validated_hello(
+    data: /* Uses the `Validated` type */ Validated<Json<HelloData>>,
+) -> Json<HelloData> {
+    Json(data.0 .0)
+}
+
+// ===========swagger config method
 fn get_docs() -> SwaggerUIConfig {
     // use rocket_okapi::settings::UrlObject;
 
@@ -27,15 +97,16 @@ fn get_docs() -> SwaggerUIConfig {
     }
 }
 
-#[rocket::main]
-async fn main() {
-    let launch_result = rocket::build()
-        .mount("/", openapi_get_routes![index])
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", openapi_get_routes![index, write_base64])
         .mount("/api", make_swagger_ui(&get_docs()))
-        .launch()
-        .await;
-    match launch_result {
-        Ok(_) => println!("Rocket shut down gracefully."),
-        Err(err) => println!("Rocket had an error: {}", err),
-    };
+        .mount("/", routes![validated_hello])
+        .register("/", catchers![rocket_validation::validation_catcher])
 }
+
+// match input_data.validate() {
+//     Ok(_) => (),
+//     Err(e) => return e;
+// };
